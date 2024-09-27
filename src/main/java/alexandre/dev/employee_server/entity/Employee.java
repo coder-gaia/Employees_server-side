894b20f4f2cee762c40deb88d1d440a26c3ca90f
coder-gaia <alexandregaia911@gmail.com>
2024-09-27 14:07:26 -0300

    Fixing the pom file

--- src/main/java/alexandre/dev/employee_server/entity/Employee.java
+++ src/main/java/alexandre/dev/employee_server/entity/Employee.java
@@ -1,12 +1,9 @@
 package alexandre.dev.employee_server.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Document(collection = "employees")
@@ -19,4 +16,36 @@ public class Employee {
     private String email;
     private String phone;
     private String department;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
 }
